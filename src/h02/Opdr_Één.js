7e3f4e8d609f2a7557ff1e59ee68b005a54bcea8
kenny01657-cmd <kmeijer@roc-dev.com>
2021-06-22 10:17:08 +0200

    .

---
+++ src/h02/Opdr_Één.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel"
+document.getElementById("subkop").innerText = "Dit is de subkop"
+document.getElementById("aap1").src = "img/aap1.png"
+document.getElementById("aap2").src = "img/aap2.png"
