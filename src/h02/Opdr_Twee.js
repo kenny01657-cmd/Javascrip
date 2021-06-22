a3974498aef6464714d97a41c35a04405f74cd16
kenny01657-cmd <kmeijer@roc-dev.com>
2021-06-22 10:56:27 +0200

    .

---
+++ src/h02/Opdr_Twee.js
@@ -0,0 +1,16 @@
+var bl1 = document.getElementById("bl1");
+bl1.style.backgroundColor = "lightblue";
+bl1.style.textAlign = "center";
+bl1.style.paddingTop = "10px";
+
+var bl2 = document.getElementById("bl2");
+bl2.style.backgroundColor = "green";
+bl2.style.marginTop = "20px";
+bl2.style.textAlign = "center";
+
+var bl3 = document.getElementById("bl3");
+bl3.style.marginTop = "40px";
+bl3.style.textAlign = "right";
+
+var bl4 = document.getElementById("bl4");
+bl4.style.backgroundColor = "red";
